bb4d539920538522ddd56c33e496a46f56da26b0
Felipe Andrada <felipe.andrada@gmail.com>
2018-05-24 00:22:22 -0300

    implemented api to verify text palindromes

---
+++ api/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/', (req, res) => {
+
+  // get text param
+  const text = req.body.text;
+  const textIsPalindrome = compareTextIsPalindrome(text);
+
+  if (textIsPalindrome) {
+    console.log('[YES]', `${text} is a palindrome`);
+    res.statusCode = 200;
+  } else {
+    console.log('[NO]', `${text} is not a palindrome`);
+    res.statusCode = 400;
+  }
+
+  res.send();
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('some error:', err);
+  }
+
+  console.log(`server is listening on ${port}`);
+});
+
+/**
+ * Compare if text is a palindrome
+ *
+ * normalize text
+ * reverses text
+ * compare the strings
+ */
+compareTextIsPalindrome = (text) => {
+  const normalizedText = normalizeText(text);
+  const reversedText = reverseText(normalizedText);
+
+  return reversedText === normalizedText;
+}
+
+/**
+ * Normalize world or phrase:
+ *
+ * set lowercase
+ * remove spaces
+ */
+normalizeText = (text) => {
+  return text.toLowerCase().replace(' ', '');
+}
+
+/**
+ * Reverses text
+ *
+ * convert to array
+ * reverses array
+ * join into new string
+ */
+reverseText = (text) => {
+  return [...text].reverse().join('');
+}
